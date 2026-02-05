a9ddcaf9981e47f1dab30535e3e16500f51843cb
Supavit <supavit.sutha@mail.kmutt.ac.th>
2026-02-05 16:19:17 +0700

    changed api link url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://bearlab-backend-d9awbhgfe4c6abdq.southeastasia-01.azurewebsites.net/api',
+  baseURL: 'https://supavit-front-bkgmbyd5hhb2bef4.eastasia-01.azurewebsites.net/api',
   // baseURL: 'http://127.0.0.1:8000/api', // Local API
   headers: {
     'Content-Type': 'application/json',
